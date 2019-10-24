125ea4d196a3bd08a7f56d86e141d46012c394d3
Lizbeth Santamaria <lizbeth.santamaria@epn.edu.ec>
2019-10-24 16:14:19 -0500

    numeros-strings

---
+++ introducción/04-numeros-strings.ts
@@ -0,0 +1,26 @@
+const numeroUno = 1; //notacion: camenlCase
+const numeroDos = 2;
+let resultado = 0;
+//SUMA
+resultado = 1 + 2;
+//RESTAR
+resultado = 1 - 2;
+//MULTIPLLICAR
+resultado = 1 * 2;
+//DIVIDIR
+resultado = 1 / 2;
+//EXPONENCIAL
+resultado = 1 ^ 2;
+//MOD
+resultado = 2 % 4; //0 -> residuo
+resultado = 4 % 3; //1 -> residuo
+resultado = 7 % 3; //1 -> residuo
+
+console.log ("pera"+2);
+
+const COLOR_ROJO = 1; //camelCase
+//NUMEROUNO     -> TODO MAYUSCULA (SEPARACION NO)
+//NUMERO_UNO    -> mayusculas guion bajo
+//NumeroUno     ->
+//numerouno
+//numero_uno
